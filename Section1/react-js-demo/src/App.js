4d4621c3c80e632f8c687308db37d5288c31a993
HarithaPS21 <harithaps59@gmail.com>
2025-10-06 17:57:54 +0530

    React Udemy Course : Section1 - React JS Demo

---
+++ Section1/react-js-demo/src/App.js
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import "./styles.css";
+
+const content = [
+  [
+    "React is extremely popular",
+    "It makes building complex, interactive UIs a breeze",
+    "It's powerful & flexible",
+    "It has a very active and versatile ecosystem",
+  ],
+  [
+    "Components, JSX & Props",
+    "State",
+    "Hooks (e.g., useEffect())",
+    "Dynamic rendering",
+  ],
+  [
+    "Official web page (react.dev)",
+    "Next.js (Fullstack framework)",
+    "React Native (build native mobile apps with React)",
+  ],
+];
+
+function App() {
+  const [activeTabIndex, setActiveTabIndex] = useState(0);
+
+  return (
+    <div>
+      <header>
+        <img src="react-logo-xs.png" alt="React logo" />
+        <div>
+          <h1>React.js</h1>
+          <p>i.e., using the React library for rendering the UI</p>
+        </div>
+      </header>
+
+      <div id="tabs">
+        <menu>
+          <button
+            className={activeTabIndex === 0 ? "active" : ""}
+            onClick={() => setActiveTabIndex(0)}
+          >
+            Why React?
+          </button>
+          <button
+            className={activeTabIndex === 1 ? "active" : ""}
+            onClick={() => setActiveTabIndex(1)}
+          >
+            Core Features
+          </button>
+          <button
+            className={activeTabIndex === 2 ? "active" : ""}
+            onClick={() => setActiveTabIndex(2)}
+          >
+            Related Resources
+          </button>
+        </menu>
+        <div id="tab-content">
+          <ul>
+            {content[activeTabIndex].map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
