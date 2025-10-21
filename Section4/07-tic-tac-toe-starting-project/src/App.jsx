bd25c8e48a2a643b2fcae20fcaa1db95a117e0c1
HarithaPS21 <harithaps59@gmail.com>
2025-10-21 18:25:38 +0530

    React-Udemy-Course : Section4/Tic-Tac-Toe-project - GameBoard component

--- Section4/07-tic-tac-toe-starting-project/src/App.jsx
+++ Section4/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,4 +1,5 @@
 import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
           <Player name="Player2" symbol="O" />
         </ol>
         {/* Game Board */}
+        <GameBoard />
       </div>
     </main>
   );
