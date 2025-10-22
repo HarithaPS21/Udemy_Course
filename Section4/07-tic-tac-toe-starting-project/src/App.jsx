99eea436506691e61b89ebe37fa1e7bec0575e9c
HarithaPS21 <harithaps59@gmail.com>
2025-10-22 14:31:02 +0530

    Section4/Tic-Tac-Toe-project - GameBoard and Log components updates

--- Section4/07-tic-tac-toe-starting-project/src/App.jsx
+++ Section4/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,14 +1,27 @@
 import { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
+import Log from "./components/Log";
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer] = useState("X");
 
-  const toggleActivePlayer = () => {
+  const handleSelectSquare = (rowIndex, colIndex) => {
     setActivePlayer((currActivePlayer) =>
       currActivePlayer === "X" ? "O" : "X"
     );
+    setGameTurns((prevTurns) => {
+      let currentPlayer = "X";
+      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+        currentPlayer = "O";
+      }
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+      return updatedTurns;
+    });
   };
 
   return (
@@ -20,11 +33,10 @@ function App() {
           <Player name="Player2" symbol="O" isActive={activePlayer === "O"} />
         </ol>
         {/* Game Board */}
-        <GameBoard
-          onSelectSquare={toggleActivePlayer}
-          activePlayerSymbol={activePlayer}
-        />
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
+      {/* Log component */}
+      <Log turns={gameTurns} />
     </main>
   );
 }
